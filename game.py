29ab70fb127d9f4f2c846c80cd8602678ecf7d74
RajjSinghh <gurrajsingh22@gmail.com>
2020-06-20 16:55:17 +0100

    added pretty board renderer

--- game.py
+++ game.py
@@ -9,9 +9,19 @@ def CreateBoard():
     return board
 
 
+def PrintBoard(board):
+    for i in range(3):
+        current_row = ""
+        for j in range(3):
+            current_row += f"|{board[i][j]}|"
+        print(current_row)
+        if i != 2:
+            print("_________")
+
+
 if __name__ == "__main__":
     board = CreateBoard()
     players = [Player("x"), Player("o")]
     players[0].MakeMove(board)
     players[1].MakeMove(board)
-    print(board)
+    PrintBoard(board)
