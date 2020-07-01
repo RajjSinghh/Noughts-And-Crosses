e79c9445f860824302f53833272317f2025f7402
RajjSinghh <gurrajsingh22@gmail.com>
2020-07-01 17:08:14 +0100

    added a check to  see who wins each game

--- game.py
+++ game.py
@@ -18,10 +18,47 @@ def PrintBoard(board):
         if i != 2:
             print("_________")
 
+def CheckWin(board):
+    winner = None
+    for row in board:
+        if row[0] == row[1] == row[2]:
+            winner = row[0]
+            break
+
+    for column in range(3):
+        if board[0][column] ==  board[1][column] == board[2][column]:
+            winner = board[0][column]
+            break
+
+    if board[0][0] == board[1][1] == board[2][2]:
+        winner = board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        winner = board[0][2]
+
+    return winner
 
 if __name__ == "__main__":
-    board = CreateBoard()
-    players = [Player("x"), Player("o")]
-    players[0].MakeMove(board)
-    players[1].MakeMove(board)
+#    board = CreateBoard()
+#    players = [Player("x"), Player("o")]
+#    players[0].MakeMove(board)
+#    players[1].MakeMove(board)
+#    PrintBoard(board)
+    board = [["x", "x", "x"], ["o", "o", "-"], ["-", "-",  "-",]]
     PrintBoard(board)
+    print(CheckWin(board))
+    print("---------------------")
+    
+    board = [["x", "-", "-"], ["x", "-", "-"], ["x", "-",  "-",]]
+    PrintBoard(board)
+    print(CheckWin(board))
+    print("---------------------")
+
+    board = [["x", "-", "o"], ["x", "o", "-"], ["o", "-",  "x",]]
+    PrintBoard(board)
+    print(CheckWin(board))
+    print("---------------------")
+
+    board = [["x", "-", "o"], ["x", "x", "-"], ["o", "-",  "x",]]
+    PrintBoard(board)
+    print(CheckWin(board))
+    print("---------------------")
